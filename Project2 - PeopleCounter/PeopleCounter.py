ba39d682fc88cf6078ef17a91cd292282ff4b456
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-08-12 17:21:56 +0900

    Update PeopleCounter.py

--- Project2 - PeopleCounter/PeopleCounter.py
+++ Project2 - PeopleCounter/PeopleCounter.py
@@ -37,8 +37,11 @@ else:
 # Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
-limits = [400,297,673,297]
-totalCount = []
+limitsUp = [103,161,296,161]
+limitsDown = [527,489,735,489]
+
+totalCountUP = []
+totalCountDown = []
 
 prev_frame_time = 0
 new_frame_time = 0  
@@ -79,7 +82,8 @@ while True:
     print(fps)
 
     resultsTracker = tracker.update(detections)
-    cv2.line(img,(limits[0],limits[1]),(limits[2],limits[3]),(0,0,255),5)
+    cv2.line(img,(limitsUp[0],limitsUp[1]),(limitsUp[2],limitsUp[3]),(0,0,255),5)
+    cv2.line(img,(limitsDown[0],limitsDown[1]),(limitsDown[2],limitsDown[3]),(0,0,255),5)
 
 
     for result in resultsTracker:
@@ -93,13 +97,19 @@ while True:
         cx,cy = x1+w//2,y1+h//2
         cv2.circle(img,(cx,cy),5,(255,0,255),cv2.FILLED)
 
-        if limits[0]<cx<limits[2] and limits[1]-15<cy<limits[1]+15:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
-                cv2.line(img,(limits[0],limits[1]),(limits[2],limits[3]),(0,255,0),5)
+        if limitsUp[0]<cx<limitsUp[2] and limitsUp[1]-15<cy<limitsUp[1]+15:
+            if totalCountUP.count(id) == 0:
+                totalCountUP.append(id)
+                cv2.line(img,(limitsUp[0],limitsUp[1]),(limitsUp[2],limitsUp[3]),(0,255,0),5)
+        
+        if limitsDown[0]<cx<limitsDown[2] and limitsDown[1]-15<cy<limitsDown[1]+15:
+            if totalCountDown.count(id) == 0:
+                totalCountDown.append(id)
+                cv2.line(img,(limitsDown[0],limitsDown[1]),(limitsDown[2],limitsDown[3]),(0,255,0),5)
 
     # cvzone.putTextRect(img, f'Count:{len(totalCount)}', (50, 50))
-    cv2.putText(img,str(len(totalCount)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
+    cv2.putText(img,str(len(totalCountUP)),(929,345),cv2.FONT_HERSHEY_PLAIN,5,(139,195,75),7)
+    cv2.putText(img,str(len(totalCountDown)),(1191,345),cv2.FONT_HERSHEY_PLAIN,5,(50,50,230),7)
  
     cv2.imshow("Image", img)
     # cv2.imshow("ImageRegion", imgRegion)
