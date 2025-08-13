e9149c5c10b4bb3c63090cb2218dc2cc02a1f1e6
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-08-13 16:33:12 +0900

    Update PPEDetection.py

--- Project3 - PPECounter/PPEDetection.py
+++ Project3 - PPECounter/PPEDetection.py
@@ -31,7 +31,6 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-            
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class Name
