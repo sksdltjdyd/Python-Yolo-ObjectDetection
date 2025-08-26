f1ee7a648ad5018bd37cf605ed66d782dc1bd2a5
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-08-26 12:14:00 +0900

    Update Extractimg.py

--- 4 - ComputerVision/Extractimg.py
+++ 4 - ComputerVision/Extractimg.py
@@ -1,11 +1,11 @@
 import cv2
-cap = cv2.VideoCapture(0) #Webcam
+cap = cv2.VideoCapture('C:/Users/User/Documents/Git/Python-Yolo-ObjectDetection/Videos/RoboFile2.mp4')
 
 count = 0
 
 while True:
     success, img = cap.read()
-    cv2.imwrite(f'C:/Users/User/Documents/Git/Python-Yolo-ObjectDetection/Extract_Ball_Images/{count}.jpg', img)
+    cv2.imwrite(f'C:/Users/User/Documents/Git/Python-Yolo-ObjectDetection/Extract_Ball_Images2/{count}.jpg', img)
     count += 1
     cv2.imshow("Original Image", img)
     cv2.waitKey(1)
